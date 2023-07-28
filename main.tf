c1db7dadd29049c03673e0763242118aa7975c18
gayatri <82455051+gayatripawar2001@users.noreply.github.com>
2023-07-28 15:25:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 provider "aws" {
   region = "ap-south-1"
 }
+terraform {
+  backend "s3" {
+  bucket = "ua-terraform-state"
+  key = "dev/ec2-app2/terraform.tfstate"
+  region = "us-east-2"
+  }
+}
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
